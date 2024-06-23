55e685ace26e9e3bc090eb6883dc2e2d4262a5af
Rattle-Brain <danialgo2001@gmail.com>
2024-06-23 22:01:52 +0200

    Prints correctly

--- userInputLoop/userInputLoop.asm
+++ userInputLoop/userInputLoop.asm
@@ -1,5 +1,6 @@
 section .bss
-    buffer resb 10    		; Reserve 10 bytes for input
+    buffer_input resb 10    		; Reserve 10 bytes for input
+    buffer_output resb 11           ; And 11 bytes to output
 
 section .data
     msg db "Enter a number: "
@@ -20,12 +21,12 @@ _start:
     ; Read input
     mov eax, 3        		; System call number (sys_read)
     mov ebx, 0        		; File descriptor (stdin)
-    mov ecx, buffer   		; Buffer to store input
+    mov ecx, buffer_input   		; Buffer to store input
     mov edx, 10       		; Number of bytes to read
     int 0x80          		; Call kernel
 
     ; Convert string to integer
-    mov ecx, buffer   		; Pointer to buffer
+    mov ecx, buffer_input   		; Pointer to buffer
     xor eax, eax      		; Clear EAX (accumulator for result)
     xor ebx, ebx      		; Clear EBX (multiplier)
 
@@ -47,32 +48,84 @@ atoi_done:
 loop_start:
     ; Print iteration number (EAX contains iteration)
     push ecx          		; Save loop counter
+    push ecx                ; Push twice. One as param to func, the other as index of loop
 
-    mov eax, 4        		; System call number (sys_write)
-    mov ebx, 1        		; File descriptor (stdout)
-
-    ; Convert counter to string
-	push edx				; Push to save and inc later
-    add edx, '0'
-    sub ecx, ecx
-    mov ecx, buffer
-    mov byte [ecx], dl
-    mov edx, 1
+    call int_to_str         ; Transform number into char[]
+        
+    add esp, 4              ; Delete top of stack
+    
+    ; Print buffer
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, buffer_output
+    mov edx, 11
     int 0x80
 
     ; Print newline
-    mov edx, 1        		; Length of newline
-    mov ecx, newline  		; Newline character
-    mov ebx, 1        		; File descriptor (stdout)
-    mov eax, 4        		; System call number (sys_write)
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, newline
+    mov edx, 1
     int 0x80
 
-	pop edx					; Restore counter to inc
-	inc edx					; Inc printable counter
-    pop ecx           		; Restore loop counter
-    loop loop_start   		; Decrement ECX and loop if not zero
+    pop ecx                 ; Retrieve counter value
+    dec ecx                 ; Decrement
+    test ecx, ecx
+    jnz loop_start
 
     ; Exit
     mov eax, 1        		; System call number (sys_exit)
     xor ebx, ebx      		; Exit code 0
     int 0x80          		; Call kernel
+
+; Definition function. Receives a number as a parameter and 
+; Transforms it into suitable text to print.
+int_to_str:
+    ; Now the state of the stack is as follows:
+    ;   ,_________________,
+    ;   |_________________|  
+    ;   |_______ret_______|  <-- esp points here
+    ;   |_______eax_______|
+    ;   |_________________|
+
+    mov eax, [esp + 4]
+
+    ; Initialize buffer pointers
+    mov edi, buffer_output + 10 ; Point to the end of the buffer
+
+    ; Clear buffer
+clear_buffer_loop:
+    mov ebx, buffer_output
+    dec edi
+    mov byte[edi], 0
+    cmp edi, ebx
+    jne clear_buffer_loop
+
+
+    mov edi, buffer_output + 10 ; Restore edi to the end of the buffer
+    mov byte [edi], 0    ; Null terminator
+
+    ; Handle zero explicitly
+    test eax, eax
+    jnz itoa_loop
+    mov byte [edi], '0'
+    jmp itoa_loop_done
+
+
+itoa_loop:
+
+    ; Convert each digit to a character
+    xor edx, edx         ; Clear edx for division
+    mov esi, 10
+    div esi              ; Divide eax by 10
+    add dl, '0'          ; Convert remainder to ASCII
+    mov [edi], dl        ; Store ASCII character
+    dec edi              ; Move to the next character
+    test eax, eax        ; Check if eax is zero
+    jnz itoa_loop        ; Continue if not zero
+
+
+itoa_loop_done:
+    ; Maybe I need to do something here.
+
+    ret
